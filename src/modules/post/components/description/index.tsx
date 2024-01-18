5dcd0b75bfdc67e04792ee41291c7fc91a880475
beksultan <bakytuulubeksultan@gmail.com>
2024-01-19 03:39:42 +0600

    added to post page Normalized Text With Br functional

--- src/modules/post/components/description/index.tsx
+++ src/modules/post/components/description/index.tsx
@@ -1,5 +1,5 @@
 import { Flex, Typography } from "antd";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const { Title } = Typography;
 
@@ -7,14 +7,42 @@ interface Props {
     text: string;
 }
 
+const getNormalizedTextWithBr = (text: string) => {
+    let word = "";
+    const arr: Array<string | React.ReactNode> = [];
+    for (let i = 0; i < text.length; i++) {
+        word += text[i];
+
+        if (word === "\n") {
+            arr.push(<br key={i} />);
+            word = "";
+        }
+        if (text[i] === " " && text[i - 1] === " ") {
+            word = word.slice(-1);
+            continue;
+        }
+        if (text[i] === " " || i === text.length - 1) {
+            arr.push(word);
+            word = "";
+        }
+    }
+    return arr;
+};
+
 const Description: React.FC<Props> = ({ text }) => {
+    const [words, setWords] = useState<Array<string | React.ReactNode>>([]);
+
+    useEffect(() => {
+        setWords(getNormalizedTextWithBr(text));
+    }, [text]);
+
     return (
         <>
             <Flex vertical gap={16}>
                 <Title level={5}>Общая информация</Title>
 
                 <Flex vertical gap={12}>
-                    {text}
+                    {words.map((word) => word)}
                 </Flex>
             </Flex>
         </>
