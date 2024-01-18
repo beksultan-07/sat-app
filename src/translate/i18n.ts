add91077bec566982d5749c0f5b857d0849d2a5e
beksultan <bakytuulubeksultan@gmail.com>
2024-01-18 09:37:28 +0600

    added ru and kg translation json and added i18n

---
+++ src/translate/i18n.ts
@@ -0,0 +1,28 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import ru from "./ru.json";
+import kg from "./kg.json";
+
+const resources = {
+    ru: {
+        translation: {
+            ...ru,
+        },
+    },
+    en: {
+        translation: {
+            ...kg,
+        },
+    },
+};
+
+i18n.use(initReactI18next).init({
+    resources,
+    lng: "ru",
+
+    interpolation: {
+        escapeValue: false,
+    },
+});
+
+export default i18n;
